d1d992c9a03874ab0ed2b0294cff080f1b8a9579
Zainul Muhammad Rofa <113880540+zainulrofa@users.noreply.github.com>
2022-09-27 09:50:13 +0700

    Update searchName.js

--- searchName.js
+++ searchName.js
@@ -1,10 +1,26 @@
-let nama = ['Abigail', "Alexandra", ' Alison',
+const nama = ['Abigail', "Alexandra", 'Alison',
     'Amanda', 'Angela', 'Bella',
     'Carol', 'Caroline', 'Carolyn',
     'Deirdre', 'Diana', 'Elizabeth',
     'Ella', 'Faith', 'Olivia', 'Penelope'
 ]
 
+function searchName(search, count, callback) {
+    let output = [];
+    let add = 0;
+    for (let i = 0; i < nama.length; i++) {
+        let namaLower = nama[i].toLowerCase()
+        if (namaLower.search(search) > -1) {
+            output.push(nama[i]);
+            add++
+            if (add == count) break
+        }
+    }
+    callback(output)
+}
 
-const filterNama = nama.filter((el, i) => el == "an", i = 0)
-console.log(filterNama)
+function callback(output) {
+    console.log(output);
+}
+
+searchName('an', 3, callback)
